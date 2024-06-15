e61abc12d694472564d88df5bb68d988273d836a
mcocenzo <mcocenzo@gmail.com>
2024-06-14 22:05:18 -0300

    web

---
+++ js/jogadores.js
@@ -0,0 +1,59 @@
+const atleta_card = document.getElementById("atleta_card")
+
+function fetchData(url) {
+    return fetch(url)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Erro ao obter os dados');
+        }
+        return response.json();
+      })
+      .then(data => {
+        return data; 
+      })
+      .catch(error => {
+        throw new Error('Houve um problema na requisição:', error);
+      });
+  }
+
+
+function criarCard(jogador) {
+    
+    const card = document.createElement('div');
+    card.classList.add('card');
+  
+    
+    const linkDetalhes = document.createElement('a');
+    linkDetalhes.href = jogador.url_detalhes;
+    
+    card.appendChild(linkDetalhes);
+  
+    
+    const imagem = document.createElement('img');
+    imagem.src = jogador.imagem;
+    imagem.alt = jogador.nome;
+    linkDetalhes.appendChild(imagem);
+  
+    
+    const nome = document.createElement('h2');
+    nome.textContent = jogador.nome;
+    linkDetalhes.appendChild(nome);
+  
+    return card;
+   
+}
+
+
+
+async function mostra_jogadores(){
+    const container = document.getElementById('container');
+    const atletas = await fetchData("https://botafogo-atletas.mange.li/2024-1/all")
+    console.log(atletas)
+    atletas.forEach(jogador => {
+        // display_card(jogador)
+        const card = criarCard(jogador);
+        container.appendChild(card);
+    });
+    }
+    
+    mostra_jogadores()
---
+++ js/script.js
@@ -0,0 +1,15 @@
+import { hex_sha256 } from "./sha256-min.mjs";
+
+const alvo = '03d570b25194224ebdaf819238cbfb4985f978b4f603257aa144ba824aefb7d0';
+const senha =  document.getElementById('senha')
+
+document.getElementById('btn_enviar').onclick = () =>{
+    const entrada = document.getElementById('senha').value;
+    if (hex_sha256(entrada) === alvo){
+        sessionStorage.setItem('logado', '1');
+        window.location.href = '/html/jogadores.html';
+    }else{
+        senha.innerHTML = 'Senha incorreta';
+        console.log("erro")
+    }
+}
